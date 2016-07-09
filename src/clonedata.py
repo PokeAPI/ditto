5174f519b539d344e6ed6a97bf350f9a0af0628f
Sargun Vohra <sargunster@gmail.com>
2016-07-09 16:40:38 -0400

    reorganize

--- src/clonedata.py
+++ src/clonedata.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 
-import sys, requests, os, os.path, json
+import json
+import os
+import os.path
+import requests
+import sys
 
-def safe_open_w(path):
-    os.makedirs(os.path.dirname(path), exist_ok=True)
-    return open(path, "w")
 
-def print_json(data, path):
-    print(json.dumps(data, indent=4, sort_keys=True), file=safe_open_w(path))
+def safe_open_w(file_name):
+    os.makedirs(os.path.dirname(file_name), exist_ok=True)
+    return open(file_name, "w")
+
+
+def print_json(data, file_name):
+    print(json.dumps(data, indent=4, sort_keys=True), file=safe_open_w(file_name))
+
 
 # Config
 
@@ -18,11 +25,11 @@ if len(sys.argv) != 3:
 baseUrl = sys.argv[1]
 targetDir = sys.argv[2]
 
-if (not baseUrl.endswith("/")):
-    baseUrl = baseUrl + "/"
+if not baseUrl.endswith("/"):
+    baseUrl += "/"
 
-if (not targetDir.endswith("/")):
-    targetDir = targetDir + "/"
+if not targetDir.endswith("/"):
+    targetDir += "/"
 
 # Root
 
@@ -38,55 +45,47 @@ print_json(endpoints.json(), path)
 for endpoint in endpoints.json().values():
     # Default index
     url = endpoint
-    resourcelist = requests.get(url)
+    resource_list = requests.get(url)
     path = targetDir + endpoint.replace(baseUrl, '') + "index.json"
     print(path)
-    print_json(resourcelist.json(), path)
-    
+    print_json(resource_list.json(), path)
+
     # Fifty index
     url = endpoint + "?limit=50"
-    resourcelist = requests.get(url)
+    resource_list = requests.get(url)
     path = targetDir + endpoint.replace(baseUrl, '') + "limit=50.json"
     print(path)
-    print_json(resourcelist.json(), path)
+    print_json(resource_list.json(), path)
 
-    # Zero index    
+    # Zero index
     url = endpoint + "?limit=0"
-    resourcelist = requests.get(url)
+    resource_list = requests.get(url)
     path = targetDir + endpoint.replace(baseUrl, '') + "limit=0.json"
     print(path)
-    print_json(resourcelist.json(), path)
-    
+    print_json(resource_list.json(), path)
+
     # All index
-    count = str(resourcelist.json()["count"])
+    count = str(resource_list.json()["count"])
     url = endpoint + "?limit=" + count
-    resourcelist = requests.get(url)
+    resource_list = requests.get(url)
     path = targetDir + endpoint.replace(baseUrl, '') + "limit=" + count + ".json"
     print(path)
-    print_json(resourcelist.json(), path)
-    
+    print_json(resource_list.json(), path)
+
     # All resources
-    for resourceSummary in resourcelist.json()['results']:
+    for resourceSummary in resource_list.json()['results']:
         resourceUrl = resourceSummary['url']
         path = targetDir + resourceUrl.replace(baseUrl, '') + "index.json"
-        
+
         if not os.path.isfile(path):
             print(path)
             resource = requests.get(resourceUrl)
             print_json(resource.json(), path)
-        
+
         if endpoint.endswith("/pokemon/"):
-            resourceUrl = resourceUrl + "encounters/"
+            resourceUrl += "encounters/"
             path = targetDir + resourceUrl.replace(baseUrl, '') + "index.json"
             if not os.path.isfile(path):
                 print(path)
                 resource = requests.get(resourceUrl)
                 print_json(resource.json(), path)
-
-
-
-
-
-
-
-
