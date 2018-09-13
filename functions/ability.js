516f1ec5d2978825f32e329b00f3f0e93c833ac5
Sargun Vohra <sargun.vohra@gmail.com>
2018-09-13 01:36:29 -0700

    log it

--- functions/ability.js
+++ functions/ability.js
@@ -9,18 +9,27 @@ exports.handler = (event, context, callback) => {
     let url = "https://pokeapi.netlify.com/api/v2/ability/index.json";
 
     getJson(url, (error, response) => {
+        console.log("error: " + error);
+        console.log("response: " + response);
+
         if (error) {
             throw "Request failed: " + url;
         }
 
         let defaultParams = {offset: 0, limit: 20};
+        console.log("defaults: " + defaultParams);
         let userParams = event.queryStringParameters;
+        console.log("user: " + userParams);
         let fullParams = Object.assign(defaultParams, userParams);
+        console.log("full: " + fullParams);
 
         let begin = fullParams.offset;
         let end = fullParams.offset + fullParams.limit;
+        console.log("from " + begin + " to " + end);
 
         let resultSlice = response.results.slice(begin, end);
+        console.log("slice: " + resultSlice);
+        
         let finalResponse = Object.assign(response, {results: resultSlice});
 
         callback(null, {statusCode: 200, body: JSON.stringify(finalResponse, null, 4)})
