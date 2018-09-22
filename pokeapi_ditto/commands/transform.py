9c29d04c206d2c436914645567fceed2801a33c0
Sargun Vohra <sargun.vohra@gmail.com>
2018-09-22 16:28:10 -0700

    Fix request by name for encounter lists

--- pokeapi_ditto/commands/transform.py
+++ pokeapi_ditto/commands/transform.py
@@ -1,10 +1,30 @@
 import json
 from pathlib import Path
-from typing import Dict
+from typing import Dict, Any
 
 from tqdm import tqdm
 
 from pokeapi_ditto.common import apply_base_url
+
+
+def _is_id(s: str):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+
+def _dump(path: Path, content: Any):
+    if not path.parent.exists():
+        path.parent.mkdir(parents=True)
+    path.write_text(json.dumps(content, sort_keys=True, indent=4))
+
+
+# TODO: blow all this up and make it good
+# this is really bade code and hard to follow
+# all this path.parent.parent nonsense is hard to understand
+# clone.py is a cleaner model to follow
 
 
 def do_transform(src_dir: str, dest_dir: str, base_url: str):
@@ -22,16 +42,21 @@ def do_transform(src_dir: str, dest_dir: str, base_url: str):
     for src_path in tqdm(list(src_paths)):
         content: Dict = json.loads(apply_base_url(src_path.read_text(), base_url))
 
+        # all files
         dest_path = dest_dir.joinpath(src_path.relative_to(src_dir))
+        _dump(dest_path, content)
 
-        if not dest_path.parent.exists():
-            dest_path.parent.mkdir(parents=True)
-        dest_path.write_text(json.dumps(content, sort_keys=True, indent=4))
-
-        if "name" in content and "id" in content:
+        # named resource files
+        if _is_id(dest_path.parent.name) and "name" in content:
             name = content["name"]
             dest_path = dest_path.parent.parent.joinpath(name, "index.json")
+            _dump(dest_path, content)
 
-            if not dest_path.parent.exists():
-                dest_path.parent.mkdir(parents=True)
-            dest_path.write_text(json.dumps(content, sort_keys=True, indent=4))
+        # a hack for pokemon/ID/encounters
+        if _is_id(dest_path.parent.parent.name) and dest_path.parent.name == "encounters":
+            pokemon_path = src_path.parent.parent.joinpath("index.json")
+            name = json.loads(pokemon_path.read_text())["name"]
+            dest_path = dest_path.parent.parent.parent.joinpath(name, "encounters", "index.json")
+            _dump(dest_path, content)
+
+
